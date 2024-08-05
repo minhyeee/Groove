3223689370d461e3823114d4bb9b52a2a275b2c8
minhyeee <alsgp01238664@gmail.com>
2024-08-06 00:22:52 +0900

    초기 설정 (DB X)

---
+++ Groove/src/main/java/org/example/groove/GrooveApplication.java
@@ -0,0 +1,13 @@
+package org.example.groove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrooveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrooveApplication.class, args);
+	}
+
+}
---
+++ Groove/src/test/java/org/example/groove/GrooveApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.groove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrooveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
